af67301de65f603c44fe0d94edc3a452ad270ca2
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-02 19:41:25 +0100

    fix: ES6 Syntax satisfied

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,11 @@
 const myFunction = function handleResponseFromAPI(promise) {
-    let myPromise = promise
-    .then(() => {
-        status: 200
-        body: "success"
-    })
+  const myPromise = promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
     .catch(() => new Error())
-    .finally(() => {console.log("Got a response from the API")});
-    return myPromise;
-}
+    .finally(() => console.log('Got a response from the API'));
+  return myPromise;
+};
 export default myFunction;
