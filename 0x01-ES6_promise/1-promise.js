ecd0de7869a5b960c3a9c64f09ad8dbc8200d577
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-02 19:11:26 +0100

    fix: ES6 Syntax passed

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,15 +1,15 @@
-const myFunction = function getFullResponseFromAPI(success){
-    const myPromise = new Promise((resolve, reject) => {
-        if (success) {
-            resolve({
-                status: 200,
-                body: 'Success'
-            })
-        } else {
-            // reject(Error("The fake API is not working currently"))
-            reject(new Error("The fake API is not working currently"))
-        }
-    });
-    return myPromise;
+const myFunction = function getFullResponseFromAPI(success) {
+  const myPromise = new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      // reject(Error("The fake API is not working currently"))
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+  return myPromise;
 };
 export default myFunction;
