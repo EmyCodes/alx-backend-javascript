43318f329d196fc362cebee30856079b4251499c
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-04 00:17:25 +0100

    update: updated the function to console.log more parameters

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,9 +4,9 @@ function handleProfileSignup() {
     const uploadPhoto_ = uploadPhoto();
     const createUser_ = createUser();
     const myPromise = Promise.all([uploadPhoto_, createUser_])
-    console.log(myPromise);
-    myPromise.then((values) => {
-        console.log(values)
+    // console.log(myPromise);
+    myPromise.then((result) => {
+        console.log(result[0].body, result[1].firstName, result[1].lastName)
     });
 }
 
