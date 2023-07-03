3009d2b96ab240b9ee66cee5bfb4f4fe2a9da1cb
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-04 00:38:06 +0100

    fix: ES6 syntax passed

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,6 +1,6 @@
 const myFunc = function signUpUser(firstName, lastName) {
-    // return new Promise(resolve, reject).resolve({firstName, lastName})
-    return Promise.resolve({firstName, lastName});
+  // return new Promise(resolve, reject).resolve({firstName, lastName})
+  return Promise.resolve({ firstName, lastName });
 };
 
 export default myFunc;
