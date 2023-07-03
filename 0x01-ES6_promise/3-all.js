491ddf729105a9343ae5d1dd691293ffc2b1932d
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-04 00:20:42 +0100

    fix: ES6 Syntax passed

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,13 @@
-import { uploadPhoto, createUser} from "./utils"
+import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-    const uploadPhoto_ = uploadPhoto();
-    const createUser_ = createUser();
-    const myPromise = Promise.all([uploadPhoto_, createUser_])
-    // console.log(myPromise);
-    myPromise.then((result) => {
-        console.log(result[0].body, result[1].firstName, result[1].lastName)
-    });
+  const uploadPhotoFunc = uploadPhoto();
+  const createUserFunc = createUser();
+  const myPromise = Promise.all([uploadPhotoFunc, createUserFunc]);
+  // console.log(myPromise);
+  myPromise.then((result) => {
+    console.log(result[0].body, result[1].firstName, result[1].lastName);
+  });
 }
-
 
 export default handleProfileSignup();
