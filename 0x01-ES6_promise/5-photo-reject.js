f74ec6d4fb3cae25e232a7f1a37ffd94a5229c2e
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-04 01:30:50 +0100

    fix: ES6 passed

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,4 +1,4 @@
 export default function uploadPhoto(filename) {
-    const myPromise = Promise.reject(new Error(`${filename} cannot be processed`));
-    return myPromise;
+  const myPromise = Promise.reject(new Error(`${filename} cannot be processed`));
+  return myPromise;
 }
