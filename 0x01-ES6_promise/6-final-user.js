f8c9f05eed8b8defd0a51060cb070935b7f6dc96
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-04 01:51:56 +0100

    update: building function

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,6 +2,7 @@ import singUpUser from "./4-user-promise.js"
 import uploadPhoto from "./utils.js"
 
 function handleProfileSignup(firstName, lastName, fileName) {
-    
+    const myPromise = Promise.all();
+    return myPromise;
 }
 export default handleProfileSignup;
