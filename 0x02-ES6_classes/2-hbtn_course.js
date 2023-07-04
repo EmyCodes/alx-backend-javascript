4aa39417926320c2e4be5ce569c5c0b8753f261b
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-04 03:40:17 +0100

    2-hbtn_course.js

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,27 @@
+export default class HolbertonCourse {
+    constructor(name, length, students) {
+        this._name = name;
+        this._length = length;
+        this._students = students; 
+    }
+    get name() {
+        return this._name;
+    }
+    set name(setName) {
+        this._name = setName;
+    }
+
+    get length() {
+        return this._length;
+    }
+    set length(setLength) {
+        this._length = setLength;
+    }
+
+    get students() {
+        return this._students;
+    }
+    set students(studentName) {
+        this._name = studentName;
+    }
+}
