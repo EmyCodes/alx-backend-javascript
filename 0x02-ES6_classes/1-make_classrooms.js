eb94f25e3e4e6940cb8292c8fdf00f55090201ef
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-04 03:23:28 +0100

    fix: ES6 Syntax passed

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,12 @@
-import Classroom from "./0-classroom.js"
+import Classroom from './0-classroom';
+
 export default function initializeRooms() {
-    const myClassroom1 = new Classroom(19);
-    const myClassroom2 = new Classroom(20);
-    const myClassroom3 = new Classroom(34);
-    
-    const myArray = [myClassroom1, myClassroom2, myClassroom3];
-    return myArray;
-    // myClassroom.push(myClassroom(19));
-    // return myClassroom;
+  const myClassroom1 = new Classroom(19);
+  const myClassroom2 = new Classroom(20);
+  const myClassroom3 = new Classroom(34);
+
+  const myArray = [myClassroom1, myClassroom2, myClassroom3];
+  return myArray;
+  // myClassroom.push(myClassroom(19));
+  // return myClassroom;
 }
