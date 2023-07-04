644b3243681f3f5d753389eaa3fe03b848180447
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-04 01:00:52 +0100

    fix: passed ALX checks

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,12 +3,14 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const uploadPhotoFunc = uploadPhoto();
   const createUserFunc = createUser();
+
   const myPromise = Promise.all([uploadPhotoFunc, createUserFunc]);
   // console.log(myPromise);
   myPromise.then((result) => {
     console.log(result[0].body, result[1].firstName, result[1].lastName);
   })
   .catch(() => console.log("Signup system offline"));
+  
   return myPromise;
 }
 
