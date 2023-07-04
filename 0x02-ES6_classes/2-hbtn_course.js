9db35ff546203a386770236c1649b5f3e5e174e6
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-04 03:44:17 +0100

    fix: ES6 syntax passed

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,27 +1,31 @@
 export default class HolbertonCourse {
-    constructor(name, length, students) {
-        this._name = name;
-        this._length = length;
-        this._students = students; 
-    }
-    get name() {
-        return this._name;
-    }
-    set name(setName) {
-        this._name = setName;
-    }
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
 
-    get length() {
-        return this._length;
-    }
-    set length(setLength) {
-        this._length = setLength;
-    }
+  get name() {
+    return this._name;
+  }
 
-    get students() {
-        return this._students;
-    }
-    set students(studentName) {
-        this._name = studentName;
-    }
+  set name(setName) {
+    this._name = setName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(setLength) {
+    this._length = setLength;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(studentName) {
+    this._name = studentName;
+  }
 }
