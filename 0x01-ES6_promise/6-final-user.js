8292d5ea32eff73508d45632ce0fc6fd924ab2b6
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-04 01:46:42 +0100

    6-final-user.js

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,7 @@
+import singUpUser from "./4-user-promise.js"
+import uploadPhoto from "./utils.js"
+
+function handleProfileSignup(firstName, lastName, fileName) {
+    
+}
+export default handleProfileSignup;
